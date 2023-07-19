bd4dc499c8d51a609c59ae9e05294101b41e0c49
Your Name <you@example.com>
2023-07-19 17:13:29 +0530

    init ial

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
-export const server = "https://shop.renewlap.com//api/v2";
-
-export const backend_url = "https://shop.renewlap.com/";
+// export const server = "http://localhost:8000/api/v2";
+// export const backend_url = "http://localhost:8000/";
+export const server = "https://api.renewlap.com/api/v2";
+export const backend_url = "https://api.renewlap.com/";
