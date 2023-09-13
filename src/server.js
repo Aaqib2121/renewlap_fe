cd387a05dbba2a0a1e50185fbe4dc456c04d4c71
Your Name <you@example.com>
2023-09-13 19:24:23 +0530

    changes

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@
 // export const backend_url = "http://localhost:8000/";
 // export const server = "https://api.renewlap.com/api/v2";
 // export const backend_url = "https://api.renewlap.com/";
-export const server = "http://renewlapbackend.netlify.app/api/v2";
+export const server = "http://localhost:8000/api/v2";
 export const backend_url = "http://renewlapbackend.netlify.app/";
 
 
